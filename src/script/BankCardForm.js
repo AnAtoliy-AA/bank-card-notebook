9cfca4708f5e65b4a55b076bcef36d999a60e4a3
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-02 22:47:49 +0300

    fix: remove comments

--- src/script/BankCardForm.js
+++ src/script/BankCardForm.js
@@ -79,6 +79,7 @@ export default class BankCardForm {
       const isCardExistInLocalStorage = checkIsBankCardExistInLocalStorage(
         cardNumberInInput
       );
+
       if (isCardValid && !isCardExistInLocalStorage) {
         const bankCardType = this.selectCardTypeFromCardNumber(
           cardNumberInInput
@@ -88,10 +89,11 @@ export default class BankCardForm {
           event.target[INPUT_NUMBERS.COMMENT_INPUT].value,
           bankCardType
         );
-        addBankCardToLocalStorage(bankCard);
         const bankCardContainer = document.querySelector(
           ".bank-card-container"
         );
+
+        addBankCardToLocalStorage(bankCard);
         bankCardContainer.appendChild(bankCard.createDomBankCard());
         domSubmitMessage.innerHTML = "";
         this.domBankCardForm.reset();
@@ -100,25 +102,21 @@ export default class BankCardForm {
     } else
       domSubmitMessage.innerHTML = `please enter 16 digits. You entered: ${cardNumberInInput.length}`;
   }
-  //https://overcoder.net/q/359800/%D1%80%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%B0-%D0%BB%D1%83%D0%BD%D0%B0
+
   checkCardValidation(cardNumber) {
-    // cardNumber as a string w/ digits only
     let digit = 0,
-      even = false; // even = n-th digit counted from the end
+      even = false;
     return (
       cardNumber
         .split("")
         .reverse()
-        .reduce(
-          (string, digitInStr) => {
-            digit = parseInt(digitInStr); // reduce arg-0 - callback fnc
-            return (
-              string +
-              ((even = !even) ? digit : [0, 2, 4, 6, 8, 1, 3, 5, 7, 9][digit])
-            );
-          }, // /end of callback fnc
-          0 // reduce arg-1 - prev value for first iteration (sum)
-        ) %
+        .reduce((string, digitInStr) => {
+          digit = parseInt(digitInStr);
+          return (
+            string +
+            ((even = !even) ? digit : [0, 2, 4, 6, 8, 1, 3, 5, 7, 9][digit])
+          );
+        }, 0) %
         10 ===
       0
     );
