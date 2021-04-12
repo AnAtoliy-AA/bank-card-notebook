82a01984b283dcd763ce546669548f85a292f01a
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-12 16:21:02 +0300

    fix: set empty object for new user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import BankCardNoteBook from './script/BankCardNoteBook';
 import './styles/main.scss';
+import BANK_CARDS_IN_LOCAL_STORAGE, { getBankCardsFromLocalStorage } from "./utilities/utilities"
 
 const createApp = () => {
+  getBankCardsFromLocalStorage();
     new BankCardNoteBook();
 }
 
